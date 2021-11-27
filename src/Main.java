f587f67631942b258112f26ccf0fa861f6770e6d
Olga Lukashenko <lukashenko.93@bk.ru>
2021-11-27 15:54:21 +0300

    Added initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 17860;
+        int bonus = 20;
+        int mili = price / bonus;
+        System.out.println(mili);
+    }
+}
